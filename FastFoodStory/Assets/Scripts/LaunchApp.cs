64e6624ea240da29b7aef34b24b96485e679f48d
abrumulticolor <abrilmulticolor@gmail.com>
2025-11-03 20:21:43 -0300

    splash changed and added images

--- FastFoodStory/Assets/Scripts/LaunchApp.cs
+++ FastFoodStory/Assets/Scripts/LaunchApp.cs
@@ -1,25 +1,16 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using UnityEngine.UI;
 
 public class LaunchApp : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        StartCoroutine(LoadGameplayScene());
-    }
 
-    // Coroutine to wait for 2 seconds and load the Gameplay scene
-    private IEnumerator LoadGameplayScene()
+    
+    public void gotogameplay()
     {
-        yield return new WaitForSeconds(2f);
         SceneManager.LoadScene("Gameplay");
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
+    
 }
