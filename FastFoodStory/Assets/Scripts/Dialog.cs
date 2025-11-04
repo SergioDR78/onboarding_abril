3e6ec4d7194c6100fa638141fbb2cdafe9fc8f32
Sergio <stonemaster78@gmail.com>
2025-11-03 21:18:36 -0300

    first game scripts added

---
+++ FastFoodStory/Assets/Scripts/Dialog.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class Dialog : MonoBehaviour
+    {
+        public string title;
+        public Sprite sprite;
+
+        public TMP_Text titleText;
+        public Image messageImage;
+
+        public void Show()
+        {
+            titleText.text = title;
+            messageImage.sprite = sprite;
+            gameObject.SetActive(true);
+        }
+    }
+}
--- FastFoodStory/Assets/Scripts/juego.cs
+++ FastFoodStory/Assets/Scripts/juego.cs
@@ -1,16 +1,54 @@
+using Assets.Scripts;
+using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class juego : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int startTimeInSeconds = 10;
+    public TMP_Text timerText;
+    
+    public GameObject pizzaPrefab;
+    
+    [Header("Order")]
+    public Dialog dialog;
+    public Sprite pizzaType;
+    
+
+
+    [Header("Customer")]
+    public GameObject customer;
+    public GameObject customerPosition;
+
     void Start()
     {
-        
+        StartCoroutine(Countdown()); // Inicia la cuenta regresiva
+        ShowCustomer(); // Muestra un comensal
+        OrderPizza(); // Ordena una pizza
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Countdown()
     {
-        
+        int currentTime = startTimeInSeconds;
+
+        while (currentTime > 0)
+        {
+            yield return new WaitForSeconds(1f);
+            timerText.text = currentTime.ToString();
+            currentTime--;
+        }        
+    }
+
+    void ShowCustomer()
+    {
+        Instantiate(customer, customerPosition.transform);
+    }
+
+    void OrderPizza() 
+    {
+        dialog.title = "Quiero una pizza de jamon y queso";
+        dialog.sprite = pizzaType;
+        dialog.Show();
+        // Lógica para ordenar pizza
     }
 }
