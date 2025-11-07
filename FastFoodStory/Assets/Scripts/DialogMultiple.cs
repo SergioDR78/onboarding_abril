796cba6bc8e23e4656eb23d090cc9ac173ba4693
Sergio <stonemaster78@gmail.com>
2025-11-07 15:42:19 -0300

    retry and quit game added

---
+++ FastFoodStory/Assets/Scripts/DialogMultiple.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class DialogMultiple : MonoBehaviour
+    {
+        public string title;
+        public TMP_Text titleText;
+
+        [Header("Button 1")]
+        public string titleButton1;
+        public Button button1;
+        public Action onButton1Click;
+
+        [Header("Button 1")]
+        public Button button2;
+        public string titleButton2;
+        public Action onButton2Click;
+
+        public void Show()
+        {
+            titleText.text = title;
+            button1.GetComponentInChildren<TMP_Text>().text = titleButton1;
+            button2.GetComponentInChildren<TMP_Text>().text = titleButton2;
+            gameObject.SetActive(true);
+            button1.onClick.AddListener( () => onButton1Click.Invoke());
+            button2.onClick.AddListener( () => onButton2Click.Invoke());
+        }
+    }
+}
--- FastFoodStory/Assets/Scripts/juego.cs
+++ FastFoodStory/Assets/Scripts/juego.cs
@@ -2,6 +2,7 @@ using Assets.Scripts;
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class juego : MonoBehaviour
 {
@@ -20,11 +21,13 @@ public class juego : MonoBehaviour
     public GameObject customer;
     public GameObject customerPosition;
 
+    [Header("Dialogs")]
+    public DialogMultiple dialogMultiple;
+
+
     void Start()
     {
-        StartCoroutine(Countdown()); // Inicia la cuenta regresiva
-        ShowCustomer(); // Muestra un comensal
-        OrderPizza(); // Ordena una pizza
+        StartLevel();
     }
 
     IEnumerator Countdown()
@@ -36,7 +39,10 @@ public class juego : MonoBehaviour
             yield return new WaitForSeconds(1f);
             timerText.text = currentTime.ToString();
             currentTime--;
-        }        
+        }
+        yield return new WaitForSeconds(1f);
+        timerText.text = "0";
+        LostLevel();
     }
 
     void ShowCustomer()
@@ -50,5 +56,29 @@ public class juego : MonoBehaviour
         dialog.sprite = pizzaType;
         dialog.Show();
         // Lógica para ordenar pizza
+    }
+
+    void LostLevel() 
+    {
+        dialogMultiple.title = "Se te acabo el tiempo!";
+        dialogMultiple.titleButton1 = "Reintentar";
+        dialogMultiple.onButton1Click = () => {
+            // Lógica para reintentar
+            StartLevel();
+        };
+        dialogMultiple.titleButton2 = "Salir";
+        dialogMultiple.onButton2Click = () => {
+            // Lógica para salir
+            SceneManager.LoadScene("Splash");
+        };
+        dialogMultiple.Show();
+    }
+
+    void StartLevel() 
+    {
+        // Lógica para iniciar el nivel
+        StartCoroutine(Countdown()); // Inicia la cuenta regresiva
+        ShowCustomer(); // Muestra un comensal
+        OrderPizza(); // Ordena una pizza
     }
 }
